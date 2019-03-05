deb0c25c88c0f40f412755c520767a0c00c92790
vincent <vincent@ogre.be>
2019-03-05 19:48:38 +0100

    hold better

--- src/0x7/script.js
+++ src/0x7/script.js
@@ -2,13 +2,14 @@
   256x256x256 - script.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2018-02-27 12:27:03
-  @Last Modified time: 2019-03-01 12:24:25
+  @Last Modified time: 2019-03-05 19:48:22
 \*----------------------------------------*/
 document.addEventListener("DOMContentLoaded", setup);
 
 function tapHandler (event){
 	top.postMessage('SUCCESS', '*');
 	console.log("tapHandler");
+	event.target.style.backgroundColor="yellow";
 }
 
 function setup(){
