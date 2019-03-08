0bb0b9e0fa5bef27b7ad74efe9dd6d062fe2c294
juptsr <juptsr@users.noreply.github.com>
2019-03-08 09:52:24 +0100

    import applis
    intéractions à faire v0.1

---
+++ src/0x6a/script.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", setup);
+
+function tapHandler (event){
+  top.postMessage('SUCCESS', '*');
+  console.log("tapHandler");
+}
+
+function setup(){
+  var scale = 1,
+      gestureArea = document.querySelector('#pinch-area'),
+      scaleElement = document.querySelector('#toPinch'),
+      resetTimeout;
+
+  interact(gestureArea)
+    .gesturable({
+      onmove: function (event) {
+        scale = scale * (1 + event.ds);
+        scaleElement.style.webkitTransform =
+        scaleElement.style.transform =
+          'scale(' + scale + ')';
+
+        
+      },
+      onend: function (event) {
+        alert("YO");
+      }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", setup);
+
+function resizedHandler(){
+  top.postMessage('SUCCESS', '*');
+  console.log("resizedHandler");
+}
+
+function setup(){
+  if (!window.DeviceOrientationEvent) {
+    return alert("NO DeviceOrientationEvent");
+  }
+
+  window.ondeviceorientation =  function(event) {
+    // alpha: rotation around z-axis
+    var rotateDegrees = ((event.alpha/360) + 1) % 1;
+    // gamma: left to right
+    var leftToRight = ((event.gamma/360) + 1) % 1;
+    // beta: front back motion
+    var frontToBack = ((event.beta/360) + 1) % 1;
+
+    drawProgress(frontToBack, document.querySelector(".x circle"));
+    drawProgress(leftToRight, document.querySelector(".y circle"));
+    drawProgress(rotateDegrees, document.querySelector(".z circle"));
+  };
+}
+
+function drawProgress(ratio, target){
+  var twoPi =  Math.PI * 2;
+  var halfPi =  Math.PI * 0.5;
+  var width = 65;
+  var angle = ratio * twoPi;
+  var radius = width * 0.5 - 5;
+  var offset = width * 0.5;
+
+  var x = offset + radius * Math.cos(angle-halfPi);
+  var y = offset + radius * Math.sin(angle-halfPi);
+  
+  target.setAttribute("cx", x)
+  target.setAttribute("cy", y)
+}
+
---
+++ src/0xd6/script.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", setup);
+var angle = 0;
+
+var chronoTimeout;
+
+function setup(){
+	///setInterval(animate, 42);
+	requestAnimationFrame(animate);
+
+	// démarrer le chrono de 3sec
+	chronoTimeout = setTimeout(timeoutHandler, 3000);
+	
+	window.onmousedown = function(){
+		clearTimeout(chronoTimeout);
+		chronoTimeout = setTimeout(timeoutHandler, 3000);
+	}
+
+	var scale = 1,
+	    gestureArea = document.querySelector('#pinch-area'),
+	    scaleElement = document.querySelector('#toPinch'),
+	    resetTimeout;
+
+	interact(gestureArea)
+	  .gesturable({
+	    onmove: function (event) {
+	      scale = scale * (1 + event.ds);
+	      scaleElement.style.webkitTransform =
+	      scaleElement.style.transform =
+	        'scale(' + scale + ')';
+
+	      
+	    },
+	    onend: function (event) {
+	      alert("YO");
+	    }
+	  });
+}
+
+function timeoutHandler(){
+	var img = document.querySelector("img");
+	img.classList.add("hide");
+	var h1 = document.querySelector("h1");
+	h1.classList.remove("hide");
+}
+
+
+function animate(){
+	angle += -10;
+	var img = document.querySelector("img");
+	img.style.transform = "rotate(" + angle + "deg)";
+	requestAnimationFrame(animate);
+}
+
+document.addEventListener("DOMContentLoaded", setup);
+
+function tapHandler (event){
+	top.postMessage('SUCCESS', '*');
+	console.log("tapHandler");
+}
+
+¨/*function setup(){
+	var scale = 1,
+	    gestureArea = document.querySelector('#pinch-area'),
+	    scaleElement = document.querySelector('#toPinch'),
+	    resetTimeout;
+
+	interact(gestureArea)
+	  .gesturable({
+	    onmove: function (event) {
+	      scale = scale * (1 + event.ds);
+	      scaleElement.style.webkitTransform =
+	      scaleElement.style.transform =
+	        'scale(' + scale + ')';
+
+	      
+	    },
+	    onend: function (event) {
+	      alert("YO");
+	    }
+	  });
+}*/
