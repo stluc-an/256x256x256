73967eabe0fca1b2d82369d5255d7ec89009b10d
Louise68 <48587876+Louise68@users.noreply.github.com>
2019-03-22 09:16:26 +0100

    0xCD
    SELECT + TIMEOUT

--- src/0x2A/script.js
+++ src/0x2A/script.js
@@ -2,22 +2,20 @@
   256x256x256 - script.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2018-02-27 12:27:03
-  @Last Modified time: 2019-01-31 12:06:22
+  @Last Modified time: 2019-01-31 12:05:55
 \*----------------------------------------*/
 document.addEventListener("DOMContentLoaded", setup);
 
 function dragMoveHandler (event){
-	
-    var target = event.target;
-    target.setAttribute('data-x', "bonjour ");
-    console.log(target.getAttribute('class'));
-    var x = (parseFloat(target.getAttribute('data-x')))+ event.dx;
-    console.log(x, y);
-        
+	var target = event.target;
+    
 	// keep the dragged position in the data-x/data-y attributes
-	 || 0) 
-	var y = (parseFloat(target.getAttribute('data-y')) || 0); 
+	var x = (parseFloat(target.getAttribute('data-x')) || -150) + event.dx;
+    var y = (parseFloat(target.getAttribute('data-y')) || -100) + event.dy;   
+    console.log(x, y);
+    
 	// translate the element
+    
 	target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 	// update the posiion attributes
 	target.setAttribute('data-x', x);
@@ -25,10 +23,14 @@ function dragMoveHandler (event){
 	console.log("dragMoveHandler");
 	top.postMessage('SUCCESS', '*');
 }
+
+
+
 function setup(){
-	interact('.trigger')
+	interact('#symboltwo')
 	.draggable({
 		// call this function on every dragmove event
 		onmove: dragMoveHandler
 	});
 }
+
---
+++ src/0xCD/script.js
@@ -0,0 +1,75 @@
+/*----------------------------------------*\
+  256x256x256 - script.js
+  @author Evrard Vincent (vincent@ogre.be)
+  @Date:   2018-02-27 12:27:03
+  @Last Modified time: 2019-03-01 13:16:26
+\*----------------------------------------*/
+document.addEventListener("DOMContentLoaded", setup);
+var angle = 0;
+
+var chronoTimeout;
+
+
+
+function setup(){
+  document.onmouseup = document.onkeyup = document.onselectionchange = getSelectionText;
+}
+function getSelectionText() {
+    var text = "";
+    var activeEl = document.activeElement;
+    var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+    if (
+      (activeElTagName == "textarea") || (activeElTagName == "input" &&
+      /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
+      (typeof activeEl.selectionStart == "number")
+    ) {
+        text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
+    } else if (window.getSelection) {
+        text = window.getSelection().toString();
+    }
+    if (text.trim() == "search") {
+ 	   console.log(text);
+     var myContainer = document.querySelector(".selectContainer");
+     myContainer.classList.add("hide");
+     startAnim();
+	}
+}
+
+
+
+function startAnim(){
+  console.log("yo");
+  ///setInterval(animate, 42);
+  var monImage =document.querySelector("img");
+  monImage.classList.remove("hide"); 
+
+ /**/ requestAnimationFrame(animate);
+
+  // démarrer le chrono de 3sec
+ /**/ chronoTimeout = setTimeout(timeoutHandler, 3000);
+  
+ /**/ window.onmousedown = function(){
+    clearTimeout(chronoTimeout);
+    chronoTimeout = setTimeout(timeoutHandler, 3000);
+  }
+}
+
+ function timeoutHandler(){
+  var img = document.querySelector("img");
+  img.classList.add("hide");
+  var h1 = document.querySelector("h1");
+  h1.classList.remove("hide");
+}
+
+function animate(){
+  angle += -10;
+  var img = document.querySelector("img");
+  img.style.transform = "rotate(" + angle + "deg)";
+  requestAnimationFrame(animate);
+}
+
+
+function final(){
+  top.postMessage('SUCCESS', '*'); 
+}
+
