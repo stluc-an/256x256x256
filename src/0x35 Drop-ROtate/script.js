9f0a7843cedb2d2e5cf6174fb85e7f13fa95978f
AurelSid <aurelsid021@gmail.com>
2019-03-22 10:03:44 +0100

    Update 0x4E / 0x35

---
+++ src/0x35 Drop-ROtate/script.js
@@ -0,0 +1,33 @@
+/*----------------------------------------*\
+  256x256x256 - script.js
+  @author Evrard Vincent (vincent@ogre.be)
+  @Date:   2018-02-27 12:27:03
+  @Last Modified time: 2019-01-31 12:18:31
+\*----------------------------------------*/
+document.addEventListener("DOMContentLoaded", setup);
+
+function dragMoveHandler (event){
+	var target = event.target,
+	// keep the dragged position in the data-x/data-y attributes
+	x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+	y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+	// translate the element
+	target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+	// update the posiion attributes
+	target.setAttribute('data-x', x);
+	target.setAttribute('data-y', y);
+}
+
+function dragEndHandler(event){
+	top.postMessage('SUCCESS', '*');
+	console.log("dragEndHandler");
+}
+
+function setup(){
+	interact('.trigger')
+	.draggable({
+		// call this function on every dragmove event
+		onmove: dragMoveHandler,
+		onend: dragEndHandler,
+	});
+}
