248eada173298ae4eda55700408c442b9a3d66cd
test <test@ogre.be>
2019-03-26 14:09:17 +0100

    2A strange stuff

--- src/0x2a/script.js
+++ src/0x2a/script.js
@@ -2,20 +2,22 @@
   256x256x256 - script.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2018-02-27 12:27:03
-  @Last Modified time: 2019-01-31 12:05:55
+  @Last Modified time: 2019-03-26 14:08:44
 \*----------------------------------------*/
 document.addEventListener("DOMContentLoaded", setup);
 
 function dragMoveHandler (event){
-	var target = event.target;
-    
-	// keep the dragged position in the data-x/data-y attributes
-	var x = (parseFloat(target.getAttribute('data-x')) || -150) + event.dx;
-    var y = (parseFloat(target.getAttribute('data-y')) || -100) + event.dy;   
+	
+    var target = event.target;
+    target.setAttribute('data-x', "bonjour ");
+    console.log(target.getAttribute('class'));
+    var x = (parseFloat(target.getAttribute('data-x')))+ event.dx;
     console.log(x, y);
-    
+        
+	// keep the dragged position in the data-x/data-y attributes
+	 || 0) 
+	var y = (parseFloat(target.getAttribute('data-y')) || 0); 
 	// translate the element
-    
 	target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 	// update the posiion attributes
 	target.setAttribute('data-x', x);
@@ -23,14 +25,10 @@ function dragMoveHandler (event){
 	console.log("dragMoveHandler");
 	top.postMessage('SUCCESS', '*');
 }
-
-
-
 function setup(){
-	interact('#symboltwo')
+	interact('.trigger')
 	.draggable({
 		// call this function on every dragmove event
 		onmove: dragMoveHandler
 	});
 }
-
