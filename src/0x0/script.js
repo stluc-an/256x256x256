c86cb1e17658187ce0d803096065825435398cd6
test <test@ogre.be>
2019-03-27 16:59:33 +0100

    hello demo
    test

--- src/0x0/script.js
+++ src/0x0/script.js
@@ -2,7 +2,7 @@
   256x256x256 - script.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2018-02-27 12:27:03
-  @Last Modified time: 2019-03-27 16:56:59
+  @Last Modified time: 2019-03-27 16:59:24
 \*----------------------------------------*/
 document.addEventListener("DOMContentLoaded", setup);
 
